b87c0a0912567134da8357e5a0e5d46cfb058d23
Gabriel Carvalho Velho <serumanovelho@gmail.com>
2023-06-30 17:19:50 -0300

    banner

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,13 @@
+import './Banner.css'
+function Banner() {
+    // JSX
+    return(
+        <header className="banner">
+            <img src="./imagens/banner.png" 
+            alt="Imagem principal da página Organo">
+            </img>
+        </header>
+    )
+}
+
+export default Banner
